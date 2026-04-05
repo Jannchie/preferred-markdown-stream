c06cf72b917881362aa2e975968ac47e1bd1c9aa
Jannchie <jannchie@gmail.com>
2026-04-06 00:27:34 +0900

    chore(eslint): add vue parser and plugins to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,23 @@
 import jannchie from '@jannchie/eslint-config'
+import tsParser from '@typescript-eslint/parser'
+import vuePlugin from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
 
 export default jannchie({
   test: true,
   vue: true,
+}).append({
+  files: ['**/*.vue'],
+  languageOptions: {
+    parser: vueParser,
+    parserOptions: {
+      ecmaVersion: 'latest',
+      extraFileExtensions: ['.vue'],
+      parser: tsParser,
+      sourceType: 'module',
+    },
+  },
+  plugins: {
+    vue: vuePlugin,
+  },
 })
